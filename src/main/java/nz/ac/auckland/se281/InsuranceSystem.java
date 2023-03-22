e3bfa5c249914ce00929e37dd3f631099ff86f46
sdes755 <sdes755@aucklanduni.ac.nz>
2023-03-22 23:02:01 +1300

    Capitalised the first letter of the username as well created error messages

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -5,6 +5,7 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class InsuranceSystem {
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
+    
   }
 
   public void printDatabase() {
@@ -12,7 +13,17 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+    
+    userName = userName.toLowerCase();
+    //userName = userName.toUpperCase(userName.charAt(0));
+    String beg = userName.substring(0,1).toUpperCase() + userName.substring(1);
+    System.out.println(beg);
+    if(beg.length()< 3){
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(beg);
+    }
+    if(Integer.parseInt(age) < 0){
+      MessageCli.INVALID_AGE.printMessage(age,beg);
+    }
   }
 
   public void loadProfile(String userName) {
