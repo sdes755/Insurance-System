9519b6d7bfd8c4428d15c289a09bd2028bb39c2c
sdes755 <sdes755@aucklanduni.ac.nz>
2023-03-22 23:32:30 +1300

    Forgot to save InsuranceSystem file so changes were not seen in the commit. But changed the if loop structure as well as called the Profiles class in the create newProfile method to add the inputted username and age.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -15,16 +15,15 @@ public class InsuranceSystem {
   public void createNewProfile(String userName, String age) {
     
     userName = userName.toLowerCase();
-    //userName = userName.toUpperCase(userName.charAt(0));
     String beg = userName.substring(0,1).toUpperCase() + userName.substring(1);
     System.out.println(beg);
     if(beg.length()< 3){
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(beg);
-    }
-    if(Integer.parseInt(age) < 0){
+    }else if(Integer.parseInt(age) < 0){
       MessageCli.INVALID_AGE.printMessage(age,beg);
     }
-    
+    Profiles profiles = new Profiles();
+    profiles.AddUsernamesandAges(userName, Integer.parseInt(age));
   }
 
   public void loadProfile(String userName) {
