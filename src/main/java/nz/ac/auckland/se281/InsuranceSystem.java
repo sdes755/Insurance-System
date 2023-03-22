099e76a999e78c5b2446236c6d4f90857c1e9b40
sdes755 <sdes755@aucklanduni.ac.nz>
2023-03-23 00:02:12 +1300

    Started commenting on Profiles.java for readability. Started working on the PRINT-DB method creating a for loop to cycle through each element in the array lists from Profiles class. Created a new method in Profiles class to count the number of profiles, which was then called in Print-db method to create the rank when printing out the MessageCli message to the system.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -7,23 +7,28 @@ public class InsuranceSystem {
     // Only this constructor can be used (if you need to initialise fields).
     
   }
-
+  Profiles profiles = new Profiles();
   public void printDatabase() {
     // TODO: Complete this method.
+    int num = profiles.NumProfiles();
+x
+    for(int i = 0; i< num; i++){
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(num, userName, age);
+    }
   }
 
   public void createNewProfile(String userName, String age) {
     
     userName = userName.toLowerCase();
     String beg = userName.substring(0,1).toUpperCase() + userName.substring(1);
-    System.out.println(beg);
+    //System.out.println(beg);
     if(beg.length()< 3){
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(beg);
     }else if(Integer.parseInt(age) < 0){
       MessageCli.INVALID_AGE.printMessage(age,beg);
+    }else{
+     profiles.AddUsernamesandAges(beg, Integer.parseInt(age));
     }
-    Profiles profiles = new Profiles();
-    profiles.AddUsernamesandAges(userName, Integer.parseInt(age));
   }
 
   public void loadProfile(String userName) {
--- src/main/java/nz/ac/auckland/se281/Profiles.java
+++ src/main/java/nz/ac/auckland/se281/Profiles.java
@@ -5,28 +5,36 @@ import java.util.ArrayList;
 
 
 public class Profiles{
-
+//Creating two array lists for Usernames and the Ages to be stored in
     private ArrayList <String> Usernames = new ArrayList<String>();
     private ArrayList<Integer> Ages = new ArrayList<Integer>();
 
 
 
-
+//Creating a method to store the Usernames and Ages in the array lists
     public void AddUsernamesandAges(String userName, int age){
-
+//Using a for loop to check if the inputted username is unique
         int truth = 1;
         for(int i = 0; i<Usernames.size(); i++){
-            
+//If it is unique, we change the truth value to 0, and it will not be added
             if(userName.equals(Usernames.get(i)) == true ){
                 truth = 0;
             }
         }
+//Checking the truth value, if 1, we add the usernames and ages. if 0, we print the error message.
         if(truth == 1){
-        Usernames.add(userName);
+            Usernames.add(userName);
         Ages.add(age);
         }  else{
             MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
         } 
 
 }
+//Creating a new method to count the number of profiles in the database
+    public int NumProfiles(){
+        int num = Usernames.size();
+        return num;
+    }
+    
+    public String Print()
 }
