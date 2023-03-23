db69996d5a39172a1738f56bdf225913ae017894
sdes755 <sdes755@aucklanduni.ac.nz>
2023-03-23 20:24:51 +1300

    Used the codestyle tool to check the formatting to make sure it is all correct. HAd mistakes which I tried to fix. Will run codestyle again to double check.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -7,42 +7,44 @@ public class InsuranceSystem {
     // Only this constructor can be used (if you need to initialise fields).
     
   }
+  
   Profiles profiles = new Profiles();
+  
   public void printDatabase() {
 //Initialising a number variable for the for print-db statements using the method in Profiles class
-    int num = profiles.Num();
+        int num = profiles.findNum();
 //Creating IF conditions to check which print-db message gets printed
-    if(num == 1){
-    MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(num),"",":");
-    }else if(num > 1){
-    MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(num),"s",":");
-    }else{
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0","s",".");
-    }
-  // After the header line gets printed, we print the databse using the method made in profiles class
-      profiles.PrintProfiles();
+        if (num == 1) {
+            MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(num),"",":");
+        } else if (num > 1) {
+            MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(num),"s",":");
+        } else {
+            MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0","s",".");
+        }
+// After the header line gets printed, we print the databse using the method made in profiles class
+        profiles.printProfiles();
   }
 
 
   public void createNewProfile(String userName, String age) {
-    //Made the input username into lowerstring, and then capitalised the first letter of the Username
-    userName = userName.toLowerCase();
-    String beg = userName.substring(0,1).toUpperCase() + userName.substring(1);
+    //Made the input username into lowerstring, then capitalised the first letter of the Username
+        userName = userName.toLowerCase();
+        String beg = userName.substring(0,1).toUpperCase() 
+        + userName.substring(1);
     
-  //Used IF conditions to check for the correct formatting of the argument inputs
+//Used IF conditions to check for the correct formatting of the argument inputs
   
-  //Using the method implemented in Profiles to check if its only numeric values in the string  
-  if(profiles.AgeFormat(age)== 0){
-      MessageCli.INVALID_AGE.printMessage(age,beg);
-    }else if(beg.length()< 3){
-      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(beg);
-
-  }else if(Integer.parseInt(age) < 0){
-      MessageCli.INVALID_AGE.printMessage(age,beg);
-    }else{
-  //If the input passes the parameters, we print the username and age into the databse using the method in the Profiles class.
-     profiles.AddUsernamesandAges(beg, Integer.parseInt(age));
-    }
+//Using the method implemented in Profiles to check if its only numeric values in the string  
+        if (profiles.ageFormat(age)== 0) {
+            MessageCli.INVALID_AGE.printMessage(age,beg);
+        } else if (beg.length()< 3) {
+            MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(beg);
+        } else if (Integer.parseInt(age) < 0) {
+            MessageCli.INVALID_AGE.printMessage(age,beg);
+        } else {
+//If the input passes the parameters, we print the username and age.
+            profiles.addUserAges(beg, Integer.parseInt(age));
+        }
   }
 
   public void loadProfile(String userName) {
--- src/main/java/nz/ac/auckland/se281/Profiles.java
+++ src/main/java/nz/ac/auckland/se281/Profiles.java
@@ -4,62 +4,66 @@ package nz.ac.auckland.se281;
 import java.util.ArrayList;
 
 
-public class Profiles{
+public class Profiles {
 //Creating two array lists for Usernames and the Ages to be stored in
     private ArrayList <String> Usernames = new ArrayList<String>();
+    
     private ArrayList<Integer> Ages = new ArrayList<Integer>();
 
 
 
 //Creating a method to store the Usernames and Ages in the array lists
-    public void AddUsernamesandAges(String userName, int age){
+  public void addUserAges(String userName, int age) {
 //Using a for loop to check if the inputted username is unique
-        int truth = 1;
-        for(int i = 0; i<Usernames.size(); i++){
+            int truth = 1;
+            
+            for(int i = 0; i<Usernames.size(); i++) {
 //If it is not unique, we change the truth value to 0, and it will not be added
-            if(userName.equals(Usernames.get(i)) == true ){
-                truth = 0;
-            }
-        }       
+                if(userName.equals(Usernames.get(i)) == true ) {
+                    truth = 0;
+                }
+            }       
 //Checking the truth value, if 1, we add the usernames and ages. if 0, we print the error message.
-        if(truth == 1){
-            Usernames.add(userName);
-            Ages.add(age);
-            MessageCli.PROFILE_CREATED.printMessage(userName,Integer.toString(age));
-        }  else{
-            MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
-        } 
-
-}
+            if(truth == 1) {
+                Usernames.add(userName);
+                Ages.add(age);
+                MessageCli.PROFILE_CREATED.printMessage(userName,Integer.toString(age));
+            } else {
+                MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+            } 
+    }
 //Creating a new method to count the number of profiles in the database
-    public void PrintProfiles(){
-        int num = Usernames.size();
-        int rank = 1;
-        //Using a for loop to run through each element in the array list and printing it out one by one in the database
-        for(int i = 0; i< num; i++){
+  public void printProfiles() {
 
-            MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(rank), Usernames.get(i),Integer.toString(Ages.get(i)));
-            rank = rank + 1;
-          }
-        }
+            int num = Usernames.size();
+            
+            int rank = 1;
+//Using a for loop to run through each element in the array list and printing it out one by one in the database
+            for(int i = 0; i< num; i++) {
+
+                MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(rank),
+                Usernames.get(i),Integer.toString(Ages.get(i)));
+                rank = rank + 1;
+            }
+    }
           
 //Created a method to count the number of profiles in the database
-    public int Num(){
-        int number = Usernames.size();
+  public int findNum() {
+            int number = Usernames.size();
 
         return number;
     }
-//Created a method to check if the inputted age is in the correct format and does not have a decimal point or character.
-    public int AgeFormat(String age){
-       //Intialising a truth value
-        int truthv = 1;
-        for(int i = 0; i < age.length(); i++ ){
-            //If not a digit, turth value is changed to zero
-            if(!Character.isDigit(age.charAt(i))){
-                truthv = 0;
+//Created a method to check if the inputted age does not have a decimal point or character.
+  public int ageFormat(String age) {
+//Intialising a truth value
+            int truthv = 1;
+            for(int i = 0; i < age.length(); i++ ) {
+//If not a digit, turth value is changed to zero
+                if(!Character.isDigit(age.charAt(i))) {
+                    truthv = 0;
+                }
             }
-        }
-        //Truth value is returned, and if 1, the age is passed.
+ //Truth value is returned, and if 1, the age is passed.
        return truthv;
     }
     
